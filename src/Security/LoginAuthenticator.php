dd26bc7051e1bc9a6d5e7b74cbfb29b26cdd7136
Luiz Smelan <luizcarlosj@gmail.com>
2023-03-08 18:48:49 +0000

    Added access control for admin routes

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -95,7 +95,7 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('_profiler_phpinfo'));
+        return new RedirectResponse($this->urlGenerator->generate('app_admin_index'));
     }
 
     protected function getLoginUrl()
